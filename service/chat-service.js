bbb18fead611932da80f5b51f0d27b73e3c8c5b7
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-01-28 20:49:12 +0200

    Persist created chats on server

--- service/chat-service.js
+++ service/chat-service.js
@@ -2,18 +2,26 @@ const uuid = require('uuid/v1');
 
 class ChatService {
 
-
-    createChat() {
-
-        var chatToken = uuid();
-
-        var result = {
-            chatToken: chatToken,
-            messages: []
-        }
-
-        return result;
+  constructor() {
+    this.chatContainer = {
+      chatCount: 0,
+      chat: new Map()
     }
+  }
+
+  createChat() {
+
+    var chatToken = uuid();
+
+    var result = {
+      chatToken: chatToken,
+      messages: []
+    }
+
+    this.chatContainer.chat.set(chatToken, result)
+    this.chatContainer.chatCount += 1
+    return result;
+  }
 
 }
 
